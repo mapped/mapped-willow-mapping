9ec5e71928af0d95a21fc7fcaa85c330149590ed
Joe Beernink <jbeernink@willowinc.com>
2023-07-12 14:15:25 -0700

    Initial version of Mappings and supporting files

---
+++ .github/workflows/scripts/restore.ps1
@@ -0,0 +1,24 @@
+Set-StrictMode -Version latest
+$ErrorActionPreference = "Stop"
+Import-Module "$PSScriptRoot/common.psm1" -Force
+$root = Get-Root
+
+foreach($solution in $(Get-Solutions)) {
+    Write-Output "Restoring '$solution' using dotnet command line." 
+
+    push-location $(Split-Path $solution -Parent)
+        Show-SDKs
+
+        dotnet restore $solution /bl:"$solution.restore.binlog" "/flp1:errorsOnly;logfile=$solution.Errors.log"
+
+        if (! $?) {
+            $rawError = $(Get-Content -Raw "$solution.Errors.log")
+            Write-Error "Failed to restore NuGet packages for $solution. Error: $rawError"
+            pop-location
+            throw
+        }
+    pop-location
+}
+
+Write-Output "Restored Packages: "
+$(get-childitem -Path $root/Ontology.Mappings/packages/ -Directory).FullName
---
+++ Ontology.Mappings/src/ObjectTransformationMatch.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="ObjectTransformationMatch.cs" company="Mapped">
+// Copyright (c) Mapped. All rights reserved.
+// </copyright>
+
+namespace Mapped.Ontology.Mappings.OntologyMapper
+{
+    /// <summary>
+    /// Indicates what type of match has occurred if any.
+    /// </summary>
+    public enum ObjectTransformationMatch
+    {
+        /// <summary>
+        /// There is no match on the property passed in at all. No use testing further by type
+        /// </summary>
+        NoPropertyMatch,
+
+        /// <summary>
+        /// There is a match on the property name, but not on the type. Try ancestors
+        /// </summary>
+        PropertyMatchOnly,
+
+        /// <summary>
+        /// There is a match on the property name and the type. We have a match
+        /// </summary>
+        PropertyAndTypeMatch,
+    }
+}
