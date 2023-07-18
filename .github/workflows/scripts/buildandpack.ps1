19824410a55dc8b75a348075c5b22a2629553441
Joe Beernink <jbeernink@willowinc.com>
2023-07-18 11:08:26 -0700

    Adding publish for nuget

--- .github/workflows/scripts/buildandpack.ps1
+++ .github/workflows/scripts/buildandpack.ps1
@@ -1,3 +1,23 @@
+param (
+    [Parameter(Mandatory)]
+    [string]$nugetSecret,
+
+    [Parameter(Mandatory)]
+    [string]$VERTUPLE,
+
+    [Parameter(Mandatory)]
+    [string]$githubRepo,
+
+    [Parameter(Mandatory)]
+    [string]$githubBranch,
+
+    [Parameter(Mandatory)]
+    [string]$githubCommit,
+
+    [Parameter(Mandatory)]
+    [string]$buildNumber
+    )
+
 Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 Import-Module "$PSScriptRoot/common.psm1" -Force
@@ -10,13 +30,17 @@ foreach($solution in $(Get-Solutions)) {
     push-location $rootPath
         Show-SDKs
 
-        dotnet pack $solution --no-restore -c Release /bl:"$solution.build.binlog" "/flp1:errorsOnly;logfile=$solution.Errors.log"
+
+        dotnet pack $solution --include-source --include-symbols --version-suffix $buildNumber --no-restore -c Release /bl:"$solution.build.binlog" "/flp1:errorsOnly;logfile=$solution.Errors.log" -p:Version=$VERTUPLE -p:AssemblyVersion=$VERTUPLE -p:FileVersion=$VERTUPLE -p:RepositoryUrl=https://github.com/$githubRepo -p:RepositoryType=git -p:RepositoryBranch=$githubBranch -p:RepositoryCommit=$githubCommit
         if (! $?) {
             $rawError = $(Get-Content -Raw "$solution.Errors.log")
             Write-Error "Failed to build $solution. Error: $rawError"
             pop-location
             throw
         }
+
+        dotnet nuget push **/*.nupkg --skip-duplicate 
+        dotnet nuget push **/*.nupkg --skip-duplicate --source https://api.nuget.org/v3/index.json --api-key $nugetSecret
 
         pop-location
 }
