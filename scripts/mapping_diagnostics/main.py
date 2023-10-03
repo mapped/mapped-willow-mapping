2579c9c0aafabfbfac65cd6bc5f8caea1b035f34
Connor Cantrell <33592545+connorjcantrell@users.noreply.github.com>
2023-10-03 09:38:58 -0700

    Add missing namespaces and relationships to gaps report

--- scripts/mapping_diagnostics/main.py
+++ scripts/mapping_diagnostics/main.py
@@ -17,15 +17,27 @@ def main():
     willow_mappings.parse()
 
     # Find gaps
-    missing_willow = set(willow_ontology.interfaces) - set(willow_mappings.interface_inputs)
-    missing_mapped = set(mapped_ontology.interfaces) - set(mapped_mappings.interface_inputs)
+    missing_willow_interfaces = set(willow_ontology.interfaces) - set(willow_mappings.interface_inputs)
+    missing_mapped_interfaces = set(mapped_ontology.interfaces) - set(mapped_mappings.interface_inputs)
+    missing_willow_relationships =  set(willow_ontology.relationships) - set(willow_mappings.relationship_inputs)
+    missing_mapped_relationships =  set(mapped_ontology.relationships) - set(mapped_mappings.relationship_inputs)
+    missing_willow_namespaces = set(willow_ontology.namespaces) - set(willow_mappings.relationship_inputs)
+    missing_mapped_namespaces = set(mapped_ontology.namespaces) - set(mapped_mappings.namespace_inputs)
 
-    missing_gaps = {
-        "missing_mapped_mappings": sorted(list(missing_mapped)),
-        "missing_willow_mappings": sorted(list(missing_willow))
+    gaps = {
+        "mapped": {
+            "missing_namespaces": sorted(list(missing_mapped_namespaces)),
+            "missing_relationships": sorted(list(missing_mapped_relationships)),
+            "missing_interfaces": sorted(list(missing_mapped_interfaces))
+        },
+        "willow": {
+            "missing_namespaces": sorted(list(missing_willow_namespaces)),
+            "missing_relationships": sorted(list(missing_willow_relationships)),
+            "missing_interfaces": sorted(list(missing_willow_interfaces))
+        }
     }
 
-    with open('mapping_gaps.json', 'w') as file:
-        json.dump(missing_gaps, file, indent=2)
+    with open('gaps.json', 'w') as file:
+        json.dump(gaps, file, indent=2)
 
 main()
