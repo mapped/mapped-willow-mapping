6140837e0e2d7972d5820588c2d00e69bed4c00f
Connor Cantrell <33592545+connorjcantrell@users.noreply.github.com>
2024-01-17 17:06:19 -0800

    Update script for ontology mapper v4.2

--- scripts/generate_mappings.py
+++ scripts/generate_mappings.py
@@ -33,29 +33,56 @@ def main():
     with open('data/willow_v1_dtdlv2_mapped.json') as file:
         willow_mappings = json.load(file)
 
+    root_mappings = {
+        "dtmi:org:brickschema:schema:Brick:Collection;1": ["dtmi:com:willowinc:Collection;1"],
+        "dtmi:org:brickschema:schema:Brick:Point;1": ["dtmi:com:willowinc:Capability;1"],
+        "dtmi:org:brickschema:schema:Brick:Command;1": ["dtmi:com:willowinc:Actuator;1"],
+        "dtmi:org:brickschema:schema:Brick:Status;1": ["dtmi:com:willowinc:State;1"],
+        "dtmi:org:brickschema:schema:Brick:Sensor;1": ["dtmi:com:willowinc:Sensor;1"],
+        "dtmi:org:brickschema:schema:Brick:Setpoint;1": ["dtmi:com:willowinc:Setpoint;1"],
+        "dtmi:org:brickschema:schema:Brick:Space;1": ["dtmi:com:willowinc:Space;1"],
+        "dtmi:org:brickschema:schema:Brick:Parameter;1": ["dtmi:com:willowinc:Parameter;1"],
+        "BilledActiveElectricalEnergy;1"
+        "dtmi:mapped:core:Thing;1": ["dtmi:com:willowinc:Asset;1"],
+        "dtmi:com:willowinc:Collection;1": ["dtmi:org:brickschema:schema:Brick:Collection;1"],
+        "dtmi:com:willowinc:Capability;1": ["dtmi:org:brickschema:schema:Brick:Point;1"],
+        "dtmi:com:willowinc:Actuator;1": ["dtmi:org:brickschema:schema:Brick:Command;1"],
+        "dtmi:com:willowinc:Sensor;1": ["dtmi:org:brickschema:schema:Brick:Sensor;1"],
+        "dtmi:com:willowinc:State;1": ["dtmi:org:brickschema:schema:Brick:Status;1"],
+        "dtmi:com:willowinc:Setpoint;1": ["dtmi:org:brickschema:schema:Brick:Setpoint;1"],
+        "dtmi:com:willowinc:Space;1": ["dtmi:org:brickschema:schema:Brick:Space;1"],
+        "dtmi:com:willowinc:Parameter;1": ["dtmi:org:brickschema:schema:Brick:Parameter;1"],
+        "dtmi:com:willowinc:Asset;1": ["dtmi:mapped:core:Thing;1"],
+        "dtmi:com:willowinc:BilledActiveElectricalEnergy;1": ["dtmi:mapped:core:Billed_Electrical_Energy_Use;1"],
+    }
+
     engine = MappingEngine(
         mapped_ontology,
         mapped_mappings,
         willow_ontology,
-        willow_mappings
+        willow_mappings,
+        root_mappings=root_mappings
     )
 
     engine.initialize_graph()
     engine.graph.add_node('dtmi:com:willowinc:airport:AirfieldLightingEquipment;1', ontology='willow')
     engine.graph.add_node('dtmi:com:willowinc:airport:Airport;1', ontology='willow')
     engine.graph.add_node('dtmi:com:willowinc:airport:AirportTerminal;1', ontology='willow')
-
-    mapped_nodes = engine.get_nodes(criteria={'relationship': 'mapsTo'})
-    unmapped_nodes = set(engine.get_nodes()) - set(mapped_nodes)
-    engine.find_optimal_mappings(list(unmapped_nodes))
-    mapped_inferred_mappings, willow_inferred_mappings, missing_mappings = engine.aggregate_mappings() 
+    _, invalid_mappings = engine.validate()
+    if invalid_mappings:
+        error_message = "Invalid mappings found for nodes:\n" + ",\n".join([f"{key} -> {value}" for key, value in sorted(invalid_mappings.items())])
+        raise Exception(error_message)
+    
+    _, inferable_nodes, uniferable_nodes = engine.classify_nodes() 
+    engine.find_optimal_mappings(inferable_nodes)
+    mapped_combined_mappings, willow_combined_mappings = engine.aggregate_mappings() 
     mapped_incoming_edges, willow_incoming_edges = engine.inspect()
 
     if not os.path.exists('scripts/output'):
         os.makedirs('scripts/output')
 
     with open('scripts/output/missing.json', 'w') as f:
-        json.dump(missing_mappings, f, indent=2)
+        json.dump(uniferable_nodes, f, indent=2)
 
     with open('scripts/output/willow_incoming_edges.json', 'w') as f:
         json.dump(willow_incoming_edges, f, indent=2)
@@ -63,8 +90,8 @@ def main():
     with open('scripts/output/mapped_incoming_edges.json', 'w') as f:
         json.dump(mapped_incoming_edges, f, indent=2)
 
-    mapped_mappings['InterfaceRemaps'] = mapped_inferred_mappings
-    willow_mappings['InterfaceRemaps'] = willow_inferred_mappings
+    mapped_mappings['InterfaceRemaps'] = mapped_combined_mappings
+    willow_mappings['InterfaceRemaps'] = willow_combined_mappings
 
     willow_dir = 'Ontologies.Mappings/src/Mappings/v1/Willow'
     mapped_dir = 'Ontologies.Mappings/src/Mappings/v1/Mapped'
