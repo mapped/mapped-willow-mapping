b70d443545ef92a7b278999445b9255a4f50b457
Joe Beernink <jbeernink@willowinc.com>
2024-04-03 15:38:22 -0700

    Updated file tests

--- Ontologies.Mappings/test/MappedMappingValidationTests.cs
+++ Ontologies.Mappings/test/MappedMappingValidationTests.cs
@@ -19,6 +19,7 @@ namespace Mapped.Ontologies.Mappings.OntologyMapper.Mapped.Test
 
         [Theory]
         [InlineData("https://mapped.com/ontologies/mapping/Willow2Mapped/latest.json", LoaderType.Http)]
+        [InlineData("..\\..\\..\\..\\src\\Mappings\\v1\\Mapped\\Willow2Mapped.json", LoaderType.File)]
         public void ValidateMappedDtmisAreValidFormat(string resourcePath, LoaderType loaderType)
         {
             var mockLogger = new Mock<ILogger>();
@@ -101,6 +102,10 @@ namespace Mapped.Ontologies.Mappings.OntologyMapper.Mapped.Test
         [InlineData("https://mapped.com/ontologies/mapping/Mapped2Willow/latest.json", true, "dtmi:org:brickschema:schema:Brick:Ablutions;1", LoaderType.Http, "dtmi:com:willowinc:Ablutions;1")]
         [InlineData("https://mapped.com/ontologies/mapping/Mapped2Willow/latest.json", false, "dtmi:org:fakeschema:schema:Brick:Ablutions;1", LoaderType.Http, null)]
         [InlineData("https://mapped.com/ontologies/mapping/Mapped2Willow/latest.json", true, "dtmi:org:brickschema:schema:Brick:CO2_Alarm_Setpoint;1", LoaderType.Http, "dtmi:com:willowinc:CO2_Alarm_Setpoint;1")]
+        [InlineData("..\\..\\..\\..\\src\\Mappings\\v1\\Willow\\Mapped2Willow.json", true, "dtmi:org:brickschema:schema:Brick:Ablutions_Room;1", LoaderType.File, "dtmi:com:willowinc:Room;1")]
+        [InlineData("..\\..\\..\\..\\src\\Mappings\\v1\\Willow\\Mapped2Willow.json", true, "dtmi:org:brickschema:schema:Brick:Ablutions;1", LoaderType.File, "dtmi:com:willowinc:Ablutions;1")]
+        [InlineData("..\\..\\..\\..\\src\\Mappings\\v1\\Willow\\Mapped2Willow.json", false, "dtmi:org:fakeschema:schema:Brick:Ablutions;1", LoaderType.File, null)]
+        [InlineData("..\\..\\..\\..\\src\\Mappings\\v1\\Willow\\Mapped2Willow.json", true, "dtmi:org:brickschema:schema:Brick:CO2_Alarm_Setpoint;1", LoaderType.File, "dtmi:com:willowinc:CO2_Alarm_Setpoint;1")]
         public void ValidateInterfaceMappings(string resourcePath, bool isFound, string input, LoaderType loaderType, string? expected)
         {
             var mockLogger = new Mock<ILogger>();
@@ -132,6 +137,10 @@ namespace Mapped.Ontologies.Mappings.OntologyMapper.Mapped.Test
         [InlineData("https://mapped.com/ontologies/mapping/Mapped2Willow/latest.json", true, "floors", LoaderType.Http, "isPartOf")]
         [InlineData("https://mapped.com/ontologies/mapping/Mapped2Willow/latest.json", true, "isLocationOf", LoaderType.Http, "locatedIn")]
         [InlineData("https://mapped.com/ontologies/mapping/Mapped2Willow/latest.json", true, "hasPoint", LoaderType.Http, "isCapabilityOf")]
+        [InlineData("..\\..\\..\\..\\src\\Mappings\\v1\\Willow\\Mapped2Willow.json", false, "isFedBy", LoaderType.File, "isFedBy")]
+        [InlineData("..\\..\\..\\..\\src\\Mappings\\v1\\Willow\\Mapped2Willow.json", true, "floors", LoaderType.File, "isPartOf")]
+        [InlineData("..\\..\\..\\..\\src\\Mappings\\v1\\Willow\\Mapped2Willow.json", true, "isLocationOf", LoaderType.File, "locatedIn")]
+        [InlineData("..\\..\\..\\..\\src\\Mappings\\v1\\Willow\\Mapped2Willow.json", true, "hasPoint", LoaderType.File, "isCapabilityOf")]
         public void ValidateRelationshipMappings(string resourcePath, bool isFound, string inputRelationship, LoaderType loaderType, string? expected)
         {
             var mockLogger = new Mock<ILogger>();
@@ -159,6 +168,7 @@ namespace Mapped.Ontologies.Mappings.OntologyMapper.Mapped.Test
 
         [Theory]
         [InlineData("https://mapped.com/ontologies/mapping/Willow2Mapped/latest.json", LoaderType.Http)]
+        [InlineData("..\\..\\..\\..\\src\\Mappings\\v1\\Mapped\\Willow2Mapped.json", LoaderType.File)]
         public void ValidateSourceDtmisAreValid(string resourcePath, LoaderType loaderType)
         {
             var mockLogger = new Mock<ILogger>();
@@ -183,6 +193,7 @@ namespace Mapped.Ontologies.Mappings.OntologyMapper.Mapped.Test
 
         [Theory]
         [InlineData("https://mapped.com/ontologies/mapping/Willow2Mapped/latest.json", LoaderType.Http)]
+        [InlineData("..\\..\\..\\..\\src\\Mappings\\v1\\Mapped\\Willow2Mapped.json", LoaderType.File)]
         public void ValidateTargetDtmisAreValid(string resourcePath, LoaderType loaderType)
         {
             var mockLogger = new Mock<ILogger>();
