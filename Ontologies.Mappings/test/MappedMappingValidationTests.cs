b3fe4049581469ceccdf4e3c050fe39622a4d4c5
Joe Beernink <jbeernink@willowinc.com>
2024-04-25 14:01:13 -0700

    Removing Airport Ontology

--- Ontologies.Mappings/test/MappedMappingValidationTests.cs
+++ Ontologies.Mappings/test/MappedMappingValidationTests.cs
@@ -179,7 +179,7 @@ namespace Mapped.Ontologies.Mappings.OntologyMapper.Mapped.Test
 
             var ontologyMappingManager = new OntologyMappingManager(resourceLoader);
             var modelParser = new ModelParser();
-            var inputDtmi = LoadDtdl("Willow.Ontology.Airport.DTDLv3.jsonld");
+            var inputDtmi = LoadDtdl("Willow.Ontology.DTDLv3.jsonld");
             var inputModels = modelParser.Parse(inputDtmi);
             ontologyMappingManager.ValidateSourceOntologyMapping(inputModels, out var invalidSources);
             Console.WriteLine(invalidSources.Count());
