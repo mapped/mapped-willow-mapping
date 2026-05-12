76d3fc7fdd39b331190ab9a7f61e9076d2396743
Connor Cantrell <connor@mapped.com>
2026-05-12 16:35:19 -0700

    add exception mapping

--- scripts/generate_mappings.py
+++ scripts/generate_mappings.py
@@ -87,6 +87,7 @@ def main():
         "dtmi:mapped:core:Pump_Skid;1": ["dtmi:com:willowinc:PumpGroup;1", "dtmi:com:willowinc:PlumbingPumpGroup;1"],
         "dtmi:com:willowinc:PumpGroup;1": ["dtmi:mapped:core:Pump_Skid;1"],
         "dtmi:com:willowinc:PlumbingPumpGroup;1": ["dtmi:mapped:core:Pump_Skid;1"],
+        "dtmi:com:willowinc:HVACPumpGroup;1": ["dtmi:mapped:core:Pump_Skid;1"],
 
         "dtmi:mapped:core:Air_Change_Rate_Sensor;1": ["dtmi:com:willowinc:AirChangesPerHour;1"],
         "dtmi:com:willowinc:AirChangesPerHour;1": ["dtmi:mapped:core:Air_Change_Rate_Sensor;1"],
